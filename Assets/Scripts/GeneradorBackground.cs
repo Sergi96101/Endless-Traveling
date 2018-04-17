908c781cb91892201fd94f04a59d49cf3cea8818
Sergi9610 <sergi.borras@enti.cat>
2018-04-17 20:15:54 +0200

    generador fons

--- Assets/Scripts/GeneradorBackground.cs
+++ Assets/Scripts/GeneradorBackground.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class GeneradorBackground : MonoBehaviour {
     private float anchoFondo;
+
+    public GameObject newFons;
+    public Transform puntGeneracio;
+
 	// Use this for initialization
 	void Start () {
-		anchoFondo = 
+        anchoFondo = GetComponent<SpriteRenderer>().size.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (transform.position.x < puntGeneracio.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + anchoFondo / 2, transform.position.y, transform.position.z);
+
+            newFons.transform.position = transform.position;
+            newFons.transform.rotation = transform.rotation;
+
+            transform.position = new Vector3(transform.position.x + anchoFondo / 2, transform.position.y, transform.position.z);
+        }
+    }
 }
