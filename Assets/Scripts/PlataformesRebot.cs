f4c869286053706d5839d42ee9282a4d6c524a83
Sergi9610 <sergi.borras@enti.cat>
2018-04-22 23:47:51 +0200

    plataformes que reboten

---
+++ Assets/Scripts/PlataformesRebot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformesRebot : MonoBehaviour {
+
+    int rebot;
+
+	// Use this for initialization
+	void Start () {
+        rebot = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(other.gameObject.GetComponent<Rigidbody2D>().velocity.x, rebot);
+        }
+    }
+}
