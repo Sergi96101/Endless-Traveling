17e7242531ccbcde782a2146657fe78ba965615c
Reivax96 <36272854+Reivax96@users.noreply.github.com>
2018-04-22 17:04:44 +0200

    Add files via upload

---
+++ Assets/Scripts/EnemyFlyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyMovement : MonoBehaviour {
+
+    private PlayerMovement thePlayer;
+
+    public float moveSpeed;
+
+    public float PlayerRange;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerMovement>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, moveSpeed = Time.deltaTime);
+		
+	}
+}
---
+++ Assets/Scripts/EnemyMovement2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement2 : MonoBehaviour {
+
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+    
+    public void Update()
+    {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+    }
+}
