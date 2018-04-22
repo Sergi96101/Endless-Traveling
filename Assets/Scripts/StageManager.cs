87ddf29a4400192ff6e86dafab2a11b6fcaff90c
Sergi9610 <sergi.borras@enti.cat>
2018-04-22 17:01:49 +0200

    versio amb canvis de zones

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour {
+
+    public GeneradorPlataformes genP;
+    public GeneradorBackground genB;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (genB.cas == GeneradorBackground.mapa.Prat)
+        {
+            genP.cas = GeneradorPlataformes.mapa.Prat;
+        }
+        else if (genB.cas == GeneradorBackground.mapa.Planeta)
+        {
+            genP.cas = GeneradorPlataformes.mapa.Planeta;
+        }
+        else
+        {
+            genP.cas = GeneradorPlataformes.mapa.Canvi;
+        }
+	}
+}
