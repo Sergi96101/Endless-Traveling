73a5668c8c0dabd20edde669db82382a2361b868
Unknown <janeudald@gmail.com>
2018-05-19 19:53:45 +0200

    Menus

---
+++ Assets/PauseMenuController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour {
+
+    public static bool GamePaused = false;
+
+    public GameObject PauseMenu;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        } 
+	}
+
+    public void Resume()
+    {
+        GamePaused = false;
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Options()
+    {
+        Debug.Log("LOADING OPTIONS...");
+    }
+}
